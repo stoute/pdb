454567522f6e49ad0fe3b4e70853960ac08d5f31
Matt Davis <matt.davis@mediacurrent.com>
2016-05-02 08:28:51 -0400

    Even more styling cleanup, mainly to systemConfig.js

--- modules/pdb_ng2/assets/app/systemConfig.js
+++ modules/pdb_ng2/assets/app/systemConfig.js
@@ -1,10 +1,10 @@
 (function (drupalSettings) {
-  "use strict";
+  'use strict';
 
   // Source: http://gomakethings.com/vanilla-javascript-version-of-jquery-extend
   // Pass in the objects to merge as arguments.
   // For a deep extend, set the first argument to `true`.
-  function extend () {
+  function extend() {
     // Variables.
     var extended = {};
     var deep = false;
@@ -20,7 +20,7 @@
     // Merge the object into the extended object.
     var merge = function (obj) {
       for (var prop in obj) {
-        if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
           // If deep merge and property is an object, merge properties.
           if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
             extended[prop] = extend(true, extended[prop], obj[prop]);
@@ -58,7 +58,9 @@
         main: 'app',
         defaultExtension: 'ts'
       },
-      angular: { defaultExtension: false }
+      angular: {
+        defaultExtension: false
+      }
     }
   };
 
