82332871371e5c03469bf6db27a753e5a8620a8c
Matt Davis <matt.davis@mediacurrent.com>
2016-11-20 14:22:31 -0500

    adding new app-aot.ts, pointing tsconfig-aot at it

---
+++ modules/pdb_ng2/assets/app/app-aot.ts
@@ -0,0 +1,43 @@
+/**
+ * @module App
+ * @preferred
+ */
+import {NgModule, SystemJsNgModuleLoader} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {ScrollLoader} from 'classes/scroll-loader';
+import {GlobalProviders} from 'classes/global-providers';
+
+// Components contains metadata about all ng2 components on the page.
+// TODO: This has got to consume a manifest w/ same info as drupalSettings.
+const components = drupalSettings.pdb.ng2.components;
+
+// Dynamically load all globally shared @Injectable services and pass as
+// providers into main app bootstrap.
+const globalProviders = new GlobalProviders(components);
+
+Promise.all(globalProviders.importGlobalInjectables())
+    .then(globalServices => {
+        // array of providers to pass into longform bootstrap to make @Injectable
+        // services shared globally.
+        let globals = globalProviders
+            .createGlobalProvidersArray(globalServices);
+
+        /**
+         * Root module for the whole application
+         */
+        @NgModule({
+            providers: [SystemJsNgModuleLoader, ...globals.globalProviders],
+            imports: [BrowserModule, ...globals.globalImports]
+        })
+        class AppModule {
+            public ngDoBootstrap(): void {}
+        }
+
+        return platformBrowserDynamic().bootstrapModule(AppModule);
+    })
+    .then(appModule => {
+        let loader = new ScrollLoader(appModule, components);
+        loader.initialize();
+    });
